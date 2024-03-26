84e0ae830247f50b479b01452c11876644effcec
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-26 11:29:20 +0500

    Exercise_15_Changing_Guest_List

---
+++ Exercise_15_Changing_Guest_List/main.ts
@@ -0,0 +1,7 @@
+let guests = ["Hamdan", "Sufiyan", "Osman", "Arham"];
+let notcoming = guests[0];
+console.log(notcoming, "is not coming");
+guests.splice(0, 1, "Huzaifa");
+guests.forEach((guest) =>
+  console.log(`Hello ${guest}, you are invited to dinner with me tomorrow at 8 PM`)
+);
