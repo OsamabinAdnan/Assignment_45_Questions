cf36e60ff7b939278201d76587420fd6b35d68ab
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-26 10:28:28 +0500

    Delete Exercise_4_FamousQuote directory
    Duplicated

--- Exercise_4_FamousQuote/main.js
+++
@@ -1,7 +0,0 @@
-//One way
-console.log(`Albert Einstein once said,"A person who never made a mistake never tried anything new."`);
-//Other way
-let author = "Albert Einstein once said";
-let quote = "A person who never made a mistake never tried anything new.";
-console.log(`${author},"${quote}"`);
-export {};
