9f15cc21adeb10447986c0c9fa8985112d4beb4f
OsamabinAdnan <160324731+OsamabinAdnan@users.noreply.github.com>
2024-03-26 10:29:26 +0500

    Delete Exercise_9_Favorite_Number directory
    Duplicated

--- Exercise_9_Favorite_Number/main.js
+++
@@ -1,4 +0,0 @@
-let favoriteNumber = 10;
-let Message = "My favorite number is";
-console.log(Message, favoriteNumber);
-export {};
--- Exercise_9_Favorite_Number/main.ts
+++
@@ -1,4 +0,0 @@
-let favoriteNumber:number = 10;
-let Message = "My favorite number is";
-console.log(Message,favoriteNumber);
-
