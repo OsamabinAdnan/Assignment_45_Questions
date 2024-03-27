73f04a33420720c632651f962dc21232566a887e
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-27 06:21:47 +0500

    Exercise_20_Think_of_something

---
+++ Exercise_20_Think_of_something/main.js
@@ -0,0 +1,9 @@
+//Making programming language array
+let programLang = ["TS", "JS", "Python", "PHP"];
+//Print the message for list
+console.log("List of Programming Languages");
+//print the value of array in the form of list
+programLang.forEach(language => {
+    console.log(language);
+});
+export {};
---
+++ Exercise_20_Think_of_something/main.ts
@@ -0,0 +1,8 @@
+//Making programming language array
+let programLang:string []= ["TS", "JS", "Python", "PHP"];
+//Print the message for list
+console.log("List of Programming Languages");
+//print the value of array in the form of list
+programLang.forEach(language =>{
+    console.log(language)
+})
