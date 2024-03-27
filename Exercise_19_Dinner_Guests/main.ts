2816f9540eb9b18403be65e78e140191d023ff40
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-27 05:55:58 +0500

    Exercise_19_Dinner_Guests

---
+++ Exercise_19_Dinner_Guests/main.ts
@@ -0,0 +1,7 @@
+let Guestlist = ["Hamdan", "Sufiyan", "Osman", "Arham", "Talha","Huzaifa"];
+
+/*Guestlist.forEach(Guest => 
+    console.log(`"Hello ${Guest}, you are invited for a dinner with me tomorrow at 8 PM"`));
+*/
+let lenghtGuests:number = Guestlist.length;
+console.log(`"The number of guests, I have invited for dinner are ${lenghtGuests}"`);
