6135a6d149233257e33751581bf4d441a3655ac6
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-31 01:16:10 +0500

    Exercise_22_Intentional_Error

---
+++ Exercise_22_Intentional_Error/main.ts
@@ -0,0 +1,22 @@
+//let make any array
+let southCountries: string[] = [
+  "Pakistan",
+  "India",
+  "Sri Lanka",
+  "Bangladesh",
+  "Nepal",
+  "Bhutan",
+  "Afghanistan",
+];
+//Producing error! by accessing  invalid index of array
+console.log(southCountries[7]);
+//Error removed
+console.log(southCountries);
+//printing 1 index at a time
+console.log(southCountries[0]);
+console.log(southCountries[1]);
+console.log(southCountries[2]);
+console.log(southCountries[3]);
+console.log(southCountries[4]);
+console.log(southCountries[5]);
+console.log(southCountries[6]);
