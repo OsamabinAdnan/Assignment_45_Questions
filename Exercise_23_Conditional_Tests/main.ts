446fbb560e065eaeec05122bc8d1b148c0a8ba67
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-31 02:23:37 +0500

    Exercise_23_Conditional_Tests

---
+++ Exercise_23_Conditional_Tests/main.ts
@@ -0,0 +1,48 @@
+let num1:number = 10;
+let num2:number = 20;
+//Test 01 
+console.log ("Test # 01");
+console.log ("Is num1 equal to 10, I predict True");
+console.log(num1 === 10);
+//Test 02
+console.log ("\nTest # 02");
+console.log ("Is num2 equal to 20, I predict True");
+console.log(num2 === 20);
+//Test 03
+console.log ("\nTest # 03");
+console.log ("Is num1 is not equal to num2, I predict True");
+console.log(num1 != num2);
+//Test 04
+console.log ("\nTest # 04");
+console.log ("Is num1 is greater than 5 && num1 is less than 15, I predict True");
+console.log(num1 >5 && num1 < 15);
+
+//Test 05
+console.log ("\nTest # 05");
+console.log ("Is num2 is greater than 5 && num2 is less than 25, I predict True");
+console.log(num2 >5 && num2 < 25);
+
+//Test 06
+console.log ("\nTest # 06");
+console.log ("Is num1 equal to num2, I predict False");
+console.log(num1 === num2);
+
+//Test 07
+console.log ("\nTest # 07");
+console.log ("Is num1 greater than 10, I predict False");
+console.log(num1 > 10);
+
+//Test 08
+console.log ("\nTest # 08");
+console.log ("Is num2 is less than 20, I predict False");
+console.log(num2 < 20);
+
+//Test 09
+console.log ("\nTest # 09");
+console.log ("Is num1 is not equal to 10, I predict False");
+console.log(num1 != 10);
+
+//Test 10
+console.log ("\nTest # 10");
+console.log ("Is num2 is not equal to 20, I predict False");
+console.log(num2 != 20);
