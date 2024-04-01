fb8a18de328f8cd97eb311461b60c9bb21532d39
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-01 07:05:38 +0500

    Exercise_29_Favorite_Fruit

---
+++ Exercise_29_Favorite_Fruit/main.js
@@ -0,0 +1,22 @@
+//Let make an array
+let favorite_fruits = ["Banana", "Mango", "Strawberry", "Peach"];
+//if conditions to check whether fruit is present in array or not
+if (favorite_fruits.includes("Banana")) {
+    console.log(`"I really like Bananas!"`);
+}
+if (favorite_fruits.includes("Mango")) {
+    console.log(`"I really like Mangos!"`);
+}
+if (favorite_fruits.includes("Strawberry")) {
+    console.log(`"I really like Strawberries!"`);
+}
+if (favorite_fruits.includes("Peach")) {
+    console.log(`"I really like Peaches!"`);
+}
+if (favorite_fruits.includes("Orange")) {
+    console.log(`"Oranges! is not present in the array"`);
+}
+if (favorite_fruits.includes("Apple")) {
+    console.log(`"Apples! is not present in the array"`);
+}
+export {};
