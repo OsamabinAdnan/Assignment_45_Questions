e6cc0f64ed50face036174564af0d4afab96e239
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-01 08:48:43 +0500

    Exercise_31_No_Users

---
+++ Exercise_31_No_Users/main.ts
@@ -0,0 +1,28 @@
+// make sure the list of users is not empty
+
+let username:string[] = ["Admin", "Hamdan", "Sufiyan", "Arham", "Osman"];
+
+//if the array is not empty
+if (username.length === 0) {
+    console.log (`"We need to find some users"`);
+} else {
+    username.forEach(user =>{
+        if (user === "Admin") {
+            console.log (`"Hello ${user}, would you like to see a status report?"`);
+        }     
+        else {
+            console.log (`"Hello ${user}, thank you for logging in again."`);    
+        } 
+    })    
+}
+
+// if the array is empty | If the list is empty, print the message We need to find some users!
+username = [];
+if (username.length === 0) {
+    console.log("We need to find some users!");//this will be the answer
+} else {
+    console.log(`Hello! ${username} how are you today` );
+}
+
+
+
