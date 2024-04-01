96ce37c038518556d23fd3f97b204ddd3a53dbe9
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-01 06:08:38 +0500

    Exercise_27_Alien_Colors_no_3

---
+++ Exercise_27_Alien_Colors_no_3/main.ts
@@ -0,0 +1,59 @@
+//First version when alien color is green
+let alien_color = "green";
+
+if (alien_color == "green") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 5 points"`);
+} else if (alien_color == "yellow") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 10 points"`);
+} else if (alien_color == "red") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 15 points"`);
+} else {
+  console.log(
+    "\nAlien color is neither green or yellow or red, player didnt earn any point"
+  );
+}
+
+//Second version when alien color is yellow
+alien_color = "yellow";
+
+if (alien_color == "green") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 5 points"`);
+} else if (alien_color == "yellow") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 10 points"`);
+} else if (alien_color == "red") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 15 points"`);
+} else {
+  console.log(
+    `\n"Alien color is neither green or yellow or red, player didnt earn any point"`
+  );
+}
+
+//Third version when alien color is red
+alien_color = "red";
+
+if (alien_color == "green") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 5 points"`);
+} else if (alien_color == "yellow") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 10 points"`);
+} else if (alien_color == "red") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 15 points"`);
+} else {
+  console.log(
+    "\nAlien color is neither green or yellow or red, player didnt earn any point"
+  );
+}
+
+//Fourth version when alien color is neither green, yellow or red
+alien_color = "blue";
+
+if (alien_color == "green") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 5 points"`);
+} else if (alien_color == "yellow") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 10 points"`);
+} else if (alien_color == "red") {
+  console.log(`\n"Yes color is ${alien_color}, player earned 15 points"`);
+} else {
+  console.log(
+    `\n"Alien color is neither green or yellow or red, player didnt earn any point"`
+  );
+}
