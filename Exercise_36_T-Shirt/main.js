8372edb01e19bfb8305ea5bbae71f1abb3bc9d9f
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-06 15:45:55 +0500

    Exercise_36_T-Shirt

---
+++ Exercise_36_T-Shirt/main.js
@@ -0,0 +1,6 @@
+function make_shirt(size, message) {
+    console.log(`"Your size of the t-shirt is ${size} and '${message}' is printed on it"`);
+}
+make_shirt("medium", "Coding is fun");
+make_shirt("small", "Osama bin Adnan");
+export {};
---
+++ Exercise_36_T-Shirt/main.ts
@@ -0,0 +1,6 @@
+
+function make_shirt(size:string, message:string) {
+    console.log (`"Your size of the t-shirt is ${size} and '${message}' is printed on it"`);
+}
+make_shirt("medium", "Coding is fun");
+make_shirt("small", "Osama bin Adnan");
