0dd25e8a80b2456d51084511c8b330b1c47b2e7c
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-06 14:42:43 +0500

    Exercise_34_Pizzas

---
+++ Exercise_34_Pizzas/main.ts
@@ -0,0 +1,9 @@
+
+let pizzas = ["Malai Boti", "Fajita Sensation", "Italiano Spicy", "Cheese Pepperoni"];
+
+//creating for loop
+
+for (let onePizza of pizzas){
+    console.log(`"I like ${onePizza} pizza"`)
+}
+console.log ('"Pizza is Love"');
