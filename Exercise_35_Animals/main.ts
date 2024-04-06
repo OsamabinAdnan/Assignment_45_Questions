b1537ccaa72b549db00b034f02f2f18d1f54ca71
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-06 15:10:45 +0500

    Exercise_35_Animals

---
+++ Exercise_35_Animals/main.ts
@@ -0,0 +1,9 @@
+//creating an array
+let animals = ["Dogs", "Cats", "Rabbits", "Goats", "Parrot"];
+
+// Using for-loop
+for (let petanimal of animals) {
+    console.log(`"${petanimal} would be a great pet"`)
+}
+//Print message outside of for-loop
+console.log ('\n"Any of these animals would make a great pet!"')
