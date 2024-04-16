e1f810a70c2284a28e4558f2e1aaf4f28e12a555
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-16 14:50:22 +0500

    Exercise_41_Magicians

---
+++ Exercise_41_Magicians/main.js
@@ -0,0 +1,9 @@
+//defining a function to print each magician name fron an array
+function show_magician(magician) {
+    magician.forEach(name => console.log(name));
+}
+// defining an array having magician name
+let magician_name = ["Harry Houdini", "David Copperfield", "David Blaine", "Uri Geller"];
+//call the function to print each magician name
+show_magician(magician_name);
+export {};
