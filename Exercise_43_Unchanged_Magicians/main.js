5952886aa22aca3967acac6ee2133172ce4d7422
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-19 23:57:00 +0500

    Exercise_43_Unchanged_Magicians

---
+++ Exercise_43_Unchanged_Magicians/main.js
@@ -0,0 +1,22 @@
+//defining a function to print each magician name fron an array
+function show_magician(magician) {
+    magician.forEach(name => console.log(name));
+}
+//Create function to make magicians great through map(), it will modify array
+function make_great(magician) {
+    return magician.map(name => `"The Great ${name}"`);
+}
+// defining an array having magician name
+let magician_name = ["Harry Houdini", "David Copperfield", "David Blaine", "Uri Geller"];
+//Making a copy of original array through slice function
+let copy_magicians_name = magician_name.slice();
+//Modify the copied array to include 'the great' with their names
+let copy_great_magicians = make_great(copy_magicians_name);
+//Showing both arrays original and copied
+//Original array
+console.log("\nOriginal Array\n");
+show_magician(magician_name);
+//Copied array
+console.log("\nCopied Array\n");
+show_magician(copy_great_magicians);
+export {};
