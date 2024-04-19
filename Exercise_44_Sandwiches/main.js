79da261484128bd6c44375e72d52f9452443e720
OsamabinAdnan <binadnanosama@gmail.com>
2024-04-20 00:47:21 +0500

    Exercise_44_Sandwiches

---
+++ Exercise_44_Sandwiches/main.js
@@ -0,0 +1,10 @@
+// Defining a function with rest parameter that accepts items arguments representing our sandwiches
+function makesandwiche(...items) {
+    console.log("\n Making a sandwiche with following ingredients\n");
+    items.forEach(item => console.log(item));
+}
+//call the function with 3 times with different argumnets
+makesandwiche("Bread", "Spread", "Filling");
+makesandwiche("Bread", "Chicken", "Cheese", "vegitable filling", "Condiments");
+makesandwiche("Bread", "Peanut Butter with chunks");
+export {};
